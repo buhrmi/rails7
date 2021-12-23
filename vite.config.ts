9823a3d910ba9f25dbd9c47765aa98af162489e5
Stefan Buhrmester <buhrmi@gmail.com>
2021-12-23 10:24:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vite'
 import ruby from 'vite-plugin-ruby'
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import assetsPreprocessor from 'svelte-assets-preprocessor'
 
-export default defineConfig({
-  plugins: [
-    ruby(),
-    svelte()
-  ],
-})
+export default ({mode}) => {
+  const prod = mode == 'production'
+  return defineConfig({
+    resolve: {
+      dedupe: ['axios']
+    },
+    plugins: [
+      ruby(),
+      svelte({
+        emitCss: prod,
+        preprocess: assetsPreprocessor({ /* options */ })
+      })
+    ],
+  })
+}
